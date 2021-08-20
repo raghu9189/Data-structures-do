f3350403d441d09fa40e656a0f1ae9a81a7e0cb1
Raghu Ballu <raghu.ballu@gmail.com>
2021-08-20 15:36:27 +0530

    Create find_num_of_digits.cpp
    Find number of digits

---
+++ find_num_of_digits.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+
+/*
+    int result=0,n=234;
+    while(n!=0){
+        result += 1;
+        n = n/10;
+    }
+    cout << "result : "<<result;
+
+*/
+    int x;
+    x = log10(2345)+1;
+    cout<<x;
+
+return 0;
+}
